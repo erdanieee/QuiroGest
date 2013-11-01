8769fd1fc19dd42524f99b8b428fab48d945fdd8
Daniel López <daniel_lopez@cbm.csic.es>
2013-11-01 22:15:54 +0100

    Actualizada listaMotivos, loader, layout. Falta unificar todas las ListFragment en uno para extender la clase.

---
+++ QuiroGest/src/main/java/dan/android/quirogest/ListViewItemClickeable.java
@@ -0,0 +1,8 @@
+package dan.android.quirogest;
+
+/**
+ * Created by dan on 1/11/13.
+ */
+public interface ListViewItemClickeable {
+    public int getListviewTag();
+}
---
+++ QuiroGest/src/main/java/dan/android/quirogest/database/CallbackItemClicked.java
@@ -0,0 +1,10 @@
+package dan.android.quirogest.database;
+
+import dan.android.quirogest.ListViewItemClickeable;
+
+/**
+ * Created by dan on 1/11/13.
+ */
+public interface CallbackItemClicked {
+    public void onListItemSelected(ListViewItemClickeable listView, long id);
+}
